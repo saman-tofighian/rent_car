963ec23ac3a4c34f9d7351808ac04491ed7bc610
Saman Tofighian <samantofighian78@gmail.com>
2025-08-02 19:58:37 +0330

    update login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,8 @@
 import Image from 'next/image';
-
 export default function Login() {
   return (
     <section className='flex justify-center items-center bg-[#d7d7d7] px-4 md:px-[8%] min-h-screen'>
       <div className='grid grid-cols-1 md:grid-cols-9 bg-white shadow-lg rounded-2xl w-full max-w-[900px] min-h-[520px] overflow-hidden'>
-        {/* راست: فرم */}
         <div className='flex flex-col justify-center items-center md:col-span-6 px-6 py-10'>
           <Image
             src='/Img/Logo.png'
@@ -21,7 +19,6 @@ export default function Login() {
               کد تایید به شماره موبایلی که وارد می‌کنید، ارسال خواهد شد.
             </p>
           </div>
-
           <form className='space-y-5 mx-auto mt-8 w-full max-w-sm'>
             <div className='flex flex-row-reverse items-center bg-[#f5f5f5] px-4 py-3 border rounded-xl'>
               <Image
@@ -38,7 +35,6 @@ export default function Login() {
                 className='flex-1 bg-transparent pr-2 outline-none text-[14px] placeholder:text-gray-400'
               />
             </div>
-
             <div className='flex items-center text-[12px] text-gray-600'>
               <input type='checkbox' className='ml-2 w-4 h-4' />
               <label>
@@ -49,7 +45,6 @@ export default function Login() {
                 را می‌پذیرم.
               </label>
             </div>
-
             <button
               type='submit'
               className='bg-[#EDEDED] py-3 rounded-xl w-full font-semibold text-[#9C9C9C] text-sm'
@@ -59,8 +54,6 @@ export default function Login() {
             </button>
           </form>
         </div>
-
-        {/* چپ: تصویر بک */}
         <div className='hidden md:block md:col-span-3'>
           <Image
             src='/Img/login.jpg'
